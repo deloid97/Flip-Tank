4f1e716829bb439d7508c5b83f4546315366a974
Derrick Loi <deloid97@gmail.com>
2016-05-15 16:05:32 -0400

    Redoing the Wave class and finishing it to work with the rest of the game

--- Flip_Tank/Wave.cs
+++ Flip_Tank/Wave.cs
@@ -2,119 +2,118 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO;
 
 namespace Flip_Tank
 {
     class Wave
     {
-        /// <summary>
-        /// Designs and controls an enemy wave based on developer-set spawn chances, waves sizes, and 
-        /// </summary>
-        /// <param name="num">Number of enemies in the wave</param>
-        /// <param name="ground">percentage chance of spawning a normal ground enemy</param>
-        /// <param name="flyer">percentage chance of spawning a normal flyer enemy</param>
-        /// <param name="grndShld">percentage chance of spawning a shielded ground enemy</param>
-        /// <param name="flyShld">percentage chance of spawning a shielded flyer enemy</param>
-        /// <param name="onScreen">Number of enemies allowed to be active at a given time</param>
-        public Wave(Stream waveFile)
+        // attributes
+        List<Enemy> waveList;
+        int wave;
+        Random rgen;
+        double flyChance; // chance of spawning flyer enemy
+        double grndChance; // chance of spawing ground enemy
+        double flyShldChance; // chance of spawning shielded flyer enemy
+        double grndShldChance; // chance of spawing shielded ground enemy
+        double totalChance; // stores the total chance to divide each by
+
+        // properties
+        public double FlyChance
         {
-            StreamReader streamReader = new StreamReader(waveFile);
+            get { return flyChance / totalChance; }
 
-            /*
-            TEXT FILE FORMAT:
-            
-            Number of Enemies
-            Number of Enemies on screen
+            set { flyChance = value; totalChance = flyChance + grndChance + flyShldChance + grndShldChance; }
+        }
 
-            Ground Chance
-            Flyer Chance
-            Shielded Ground Chance
-            Shielded Flyer Chance*/
+        public double GroundChance
+        {
+            get { return grndChance / totalChance; }
 
-            num = int.Parse(streamReader.ReadLine());
-            onScreen = int.Parse(streamReader.ReadLine());
-            ground = int.Parse(streamReader.ReadLine());
-            flyer = int.Parse(streamReader.ReadLine());
-            grndShld = int.Parse(streamReader.ReadLine());
-            flyShld = int.Parse(streamReader.ReadLine());
+            set { grndChance = value; totalChance = flyChance + grndChance + flyShldChance + grndShldChance; }
+        }
 
-            rng = new Random();
-            activate = false;
-           
-            waveRoster = new Enemy[this.num];
-            enemyType = new EType[this.num];
+        public double FlyShieldChance
+        {
+            get { return flyShldChance / totalChance; }
 
-            //integers used in calculating spawn chance
-            int chance1 = ground;
-            int chance2 = chance1 + flyer;
-            int chance3 = chance2 + grndShld;
-            int chance4 = chance3 + flyShld;
-            
-            //loop builds the enemy wave
-            for (int i = 0; i < waveRoster.Length; i++)
+            set { flyShldChance = value; totalChance = flyChance + grndChance + flyShldChance + grndShldChance; }
+        }
+
+        public double GroundShieldChance
+        {
+            get { return grndShldChance / totalChance; }
+
+            set { grndShldChance = value; totalChance = flyChance + grndChance + flyShldChance + grndShldChance; }
+        }
+
+        public List<Enemy> WaveList
+        {
+            get { return waveList; }
+        }
+
+        public Wave()
+        {
+            wave = 0;
+            rgen = new Random();
+            waveList = new List<Enemy>();
+
+            flyChance = 25;
+            grndChance = 25;
+            flyShldChance = 25;
+            grndShldChance = 25;
+        }
+
+        public void NewWave(int waveNum, int enemyNum, double flyChn, double grndChn, double flyShldChn, double grndShldChn)
+        {
+
+        }
+
+        // spawns a specific type of enemy
+        public void SpawnEnemy(string type)
+        {
+            if(type.ToLower() == "flyer")
             {
-                int chance = rng.Next(chance4);
-
-                if (chance < chance1 && chance >= 0)
-                {
-                    waveRoster[i] = new Ground();
-                    enemyType[i] = EType.Ground;
-                }
-                if (chance < chance2 && chance >= chance1)
-                {
-                    waveRoster[i] = new Flyer();
-                    enemyType[i] = EType.Flyer;
-                }
-                if (chance < chance3 && chance >= chance2)
-                {
-                    waveRoster[i] = new GroundShield();
-                    enemyType[i] = EType.GroundShield;
-                }
-                if (chance < chance4 && chance >= chance3)
-                {
-                    waveRoster[i] = new FlyerShield();
-                    enemyType[i] = EType.FlyerShield;
-                }
+                waveList.Add(new Flyer());
+            }
+            if (type.ToLower() == "ground")
+            {
+                waveList.Add(new Ground());
+            }
+            if (type.ToLower() == "shield")
+            {
+                waveList.Add(new FlyerShield());
+            }
+            if (type.ToLower() == "groundshield")
+            {
+                waveList.Add(new GroundShield());
             }
         }
 
-        enum EType { Ground, Flyer, GroundShield, FlyerShield }; //enum for enemy types used in casting enemies from the waveRoster array
-
-        private int num;
-        private int ground;
-        private int flyer;
-        private int grndShld;
-        private int flyShld;
-        private int onScreen;
-
-        private Random rng;
-        private Enemy[] waveRoster; //stores all the enemies in the wave
-        private EType[] enemyType; //store the type of enemy in waveRoster for use of casting
-
-        private bool activate; //controls whether the wave is allowed to spawn in enemies
-        public bool Activate
+        // spawns a random enemy
+        public void SpawnEnemy()
         {
-            get { return activate; }
-        }
+            int spawnType = rgen.Next(100);
+            double trueFC = flyChance / totalChance * 100;
+            double trueGC = grndChance / totalChance * 100;
+            double trueFSC = flyShldChance / totalChance * 100;
+            double trueGSC = grndShldChance / totalChance * 100;
 
-        public void BeginWave() //initiates Wave and allows enemies to spawn
-        {
-            activate = true;
-        }
-
-        public void EndWave() //ends the wave and deactivates all of wave's enemies
-        {
-            activate = false;
-            for(int i = 0; i < waveRoster.Length; i++)
+            if(spawnType >= 0 && spawnType < trueFC)
             {
-              //  waveRoster[i].Deactivate();
+                waveList.Add(new Flyer());
             }
-        }
-
-        public void Spawn() //spawns enemies if wave is active and there is are fewer enemies than the allowed amount
-        {
-           // if(Activate && /*condition to check if enemies active is fewer than int OnScreen*/)
+            if(spawnType >= trueFC && spawnType < trueFC + trueGC)
+            {
+                waveList.Add(new Ground());
+            }
+            if (spawnType >= trueFC + trueGC && spawnType < trueFC + trueGC + trueFSC)
+            {
+                waveList.Add(new FlyerShield());
+            }
+            if (spawnType >= trueFC + trueGC + trueFSC && spawnType < trueFC + trueGC + trueFSC + trueGSC)
+            {
+                waveList.Add(new GroundShield());
+            }
         }
     }
 }
---
+++ Flip_Tank/Wave_B.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Flip_Tank
+{
+    class Wave_B
+    {
+        /// <summary>
+        /// Designs and controls an enemy wave based on developer-set spawn chances, waves sizes, and 
+        /// </summary>
+        /// <param name="num">Number of enemies in the wave</param>
+        /// <param name="ground">percentage chance of spawning a normal ground enemy</param>
+        /// <param name="flyer">percentage chance of spawning a normal flyer enemy</param>
+        /// <param name="grndShld">percentage chance of spawning a shielded ground enemy</param>
+        /// <param name="flyShld">percentage chance of spawning a shielded flyer enemy</param>
+        /// <param name="onScreen">Number of enemies allowed to be active at a given time</param>
+        public Wave_B(Stream waveFile)
+        {
+            StreamReader streamReader = new StreamReader(waveFile);
+
+            /*
+            TEXT FILE FORMAT:
+            
+            Number of Enemies
+            Number of Enemies on screen
+
+            Ground Chance
+            Flyer Chance
+            Shielded Ground Chance
+            Shielded Flyer Chance*/
+
+            num = int.Parse(streamReader.ReadLine());
+            onScreen = int.Parse(streamReader.ReadLine());
+            ground = int.Parse(streamReader.ReadLine());
+            flyer = int.Parse(streamReader.ReadLine());
+            grndShld = int.Parse(streamReader.ReadLine());
+            flyShld = int.Parse(streamReader.ReadLine());
+
+            rng = new Random();
+            activate = false;
+           
+            waveRoster = new Enemy[this.num];
+            enemyType = new EType[this.num];
+
+            //integers used in calculating spawn chance
+            int chance1 = ground;
+            int chance2 = chance1 + flyer;
+            int chance3 = chance2 + grndShld;
+            int chance4 = chance3 + flyShld;
+            
+            //loop builds the enemy wave
+            for (int i = 0; i < waveRoster.Length; i++)
+            {
+                int chance = rng.Next(chance4);
+
+                if (chance < chance1 && chance >= 0)
+                {
+                    waveRoster[i] = new Ground();
+                    enemyType[i] = EType.Ground;
+                }
+                if (chance < chance2 && chance >= chance1)
+                {
+                    waveRoster[i] = new Flyer();
+                    enemyType[i] = EType.Flyer;
+                }
+                if (chance < chance3 && chance >= chance2)
+                {
+                    waveRoster[i] = new GroundShield();
+                    enemyType[i] = EType.GroundShield;
+                }
+                if (chance < chance4 && chance >= chance3)
+                {
+                    waveRoster[i] = new FlyerShield();
+                    enemyType[i] = EType.FlyerShield;
+                }
+            }
+        }
+
+        enum EType { Ground, Flyer, GroundShield, FlyerShield }; //enum for enemy types used in casting enemies from the waveRoster array
+
+        private int num;
+        private int ground;
+        private int flyer;
+        private int grndShld;
+        private int flyShld;
+        private int onScreen;
+
+        private Random rng;
+        private Enemy[] waveRoster; //stores all the enemies in the wave
+        private EType[] enemyType; //store the type of enemy in waveRoster for use of casting
+
+        private bool activate; //controls whether the wave is allowed to spawn in enemies
+        public bool Activate
+        {
+            get { return activate; }
+        }
+
+        public void BeginWave() //initiates Wave and allows enemies to spawn
+        {
+            activate = true;
+        }
+
+        public void EndWave() //ends the wave and deactivates all of wave's enemies
+        {
+            activate = false;
+            for(int i = 0; i < waveRoster.Length; i++)
+            {
+              //  waveRoster[i].Deactivate();
+            }
+        }
+
+        public void Spawn() //spawns enemies if wave is active and there is are fewer enemies than the allowed amount
+        {
+           // if(Activate && /*condition to check if enemies active is fewer than int OnScreen*/)
+        }
+    }
+}
